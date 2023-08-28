ae450e5e06eb6e6989ddf0d3b0fac95d912f03de
utsha sinha <utsha.sinha@uber.com>
2023-08-28 16:03:37 +0530

    updating catalog service to include service layer

--- catalog/handler/handler.go
+++ catalog/handler/handler.go
@@ -1,26 +1,41 @@
 package handler
 
 import (
+	"context"
 	"distributed-systems-ghc/catalog/models"
 	"distributed-systems-ghc/catalog/service"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"log"
 	"net/http"
+	"time"
 )
 
 // GetCatalog is the api used to get the saree catalog
 func GetCatalog(c *gin.Context) {
 
-	requestBody, err := c.GetRawData()
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to read request body"})
+	//requestBody, err := c.GetRawData()
+	//if err != nil {
+	//	c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to read request body"})
+	//	return
+	//}
+	//err = json.Unmarshal(requestBody, &getCatalogResponse)
+
+	var getCatalogResponse *models.CatalogResponse
+	if err := c.ShouldBindJSON(&getCatalogResponse); err != nil {
+		log.Printf("Error while parsing order data: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	var getCatalogResponse *models.CatalogResponse
-	err = json.Unmarshal(requestBody, &getCatalogResponse)
 
-	sareesByCategory, err := service.GetCatalogByCategory(getCatalogResponse.CategoryID, getCatalogResponse.ColorID)
+	// Create a context with a timeout of 5 seconds
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	db, _ := c.Get("db")
+
+	sareesByCategory, err := service.GetCatalogByCategory(db.(*gorm.DB), getCatalogResponse.CategoryID)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "error getting the catalog"})
 		return
@@ -50,18 +65,15 @@ func UpdateCatalog(c *gin.Context) {
 
 	var updateCatalogResponse *models.CatalogResponse
 	err = json.Unmarshal(requestBody, &updateCatalogResponse)
+	// Create a context with a timeout of 5 seconds
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	sareesByCategory, err := service.UpdateCatalogByCategory(updateCatalogResponse.CategoryID, updateCatalogResponse.ColorID)
+	db, _ := c.Get("db")
+	err = service.UpdateCatalogByCategory(db.(*gorm.DB), updateCatalogResponse.CategoryID, updateCatalogResponse.Quantity)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "error updating the catalog"})
 		return
-	}
-
-	result, _ := json.Marshal(sareesByCategory)
-
-	var sarees []*models.Product
-	if err = json.Unmarshal(result, &sarees); err != nil {
-		log.Fatal(err)
 	}
 
 	c.Header("Content-Type", "application/json")
--- catalog/service/service.go
+++ catalog/service/service.go
@@ -1,8 +1,26 @@
 package service
 
-func GetCatalogByCategory(category int32, color int32) (response interface{}, err error) {
-	return nil, nil
+import (
+	"distributed-systems-ghc/catalog/models"
+	"distributed-systems-ghc/catalog/repository"
+	"gorm.io/gorm"
+)
+
+func GetCatalogByCategory(db *gorm.DB, category int) (response *models.Inventory, err error) {
+	inventoryRepo := &repository.InventoryRepository{DB: db} // Initialize with actual repository
+	inventory, err := inventoryRepo.GetInventoryByProductID(category)
+	if err != nil {
+		return &models.Inventory{}, err
+	}
+
+	return inventory, nil
 }
-func UpdateCatalogByCategory(category int32, color int32) (response interface{}, err error) {
-	return nil, nil
+func UpdateCatalogByCategory(db *gorm.DB, category int, qty int) (err error) {
+	inventoryRepo := &repository.InventoryRepository{DB: db} // Initialize with actual repository
+
+	err = inventoryRepo.UpdateInventory(&models.Inventory{ProductID: category, StockQty: qty})
+	if err != nil {
+		return err
+	}
+	return nil
 }
