95283aff5ecd0f2bd5956fe9f6a4c8722e626103
Sanya Sharma <sanya.sharma@uber.com>
2023-09-04 00:21:45 +0530

    adding rollback payment functionality

--- payment/service/payment-methods/amex.go
+++ payment/service/payment-methods/amex.go
@@ -10,3 +10,8 @@ func (p *Amex) Execute() (error) {
 	duration := 2 * time.Second
     time.Sleep(duration)
 }
+
+func (p *Amex) Cancel() (error) {
+	duration := 5 * time.Second
+    time.Sleep(duration)
+}
--- payment/service/payment-methods/weiss.go
+++ payment/service/payment-methods/weiss.go
@@ -10,3 +10,8 @@ func (p *Weiss) Execute() (error) {
 	duration := 4 * time.Second
     time.Sleep(duration)
 }
+
+func (p *Weiss) Cancel() (error) {
+	duration := 3 * time.Second
+    time.Sleep(duration)
+}
