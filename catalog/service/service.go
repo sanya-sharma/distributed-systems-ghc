46c71483919b848a817b3ec0f60554abceba8186
Sanya Sharma <sanya.sharma@uber.com>
2023-09-19 22:36:10 +0530

    Adding retries in db call - catalog service

--- catalog/service/service.go
+++ catalog/service/service.go
@@ -6,21 +6,44 @@ import (
 	"gorm.io/gorm"
 )
 
+// GetCatalogs fetches the catalog from DB and returns it
 func GetCatalog(db *gorm.DB) (response []models.Catalog, err error) {
 	catalogRepo := &repository.CatalogRepository{DB: db} // Initialize with actual repository
-	catalog, err := catalogRepo.GetCatalog()
+
+	for retry := 0; retry <= maxRetries; retry++ {
+		response, err := catalogRepo.GetCatalog()
+		if err == nil {
+			break
+		}
+
+		// Log the retry and sleep before the next attempt
+		log.Printf("Retrying GetCatalog from DB, attempt %d", retry+1)
+		time.Sleep(time.Second * time.Duration(retry+1))
+	}
 	if err != nil {
-		return nil, err
+		return response, err
 	}
 
-	return catalog, nil
+	return response, nil
 }
+
+// UpdateCatalogByCategory updates the catalog DB for given product ID and stockQty
 func UpdateCatalogByCategory(db *gorm.DB, category int, qty int) (err error) {
 	catalogRepo := &repository.CatalogRepository{DB: db} // Initialize with actual repository
 
-	err = catalogRepo.UpdateCatalog(&models.Catalog{ProductID: category, StockQty: qty})
+	for retry := 0; retry <= maxRetries; retry++ {
+		err := catalogRepo.UpdateCatalog(&models.Catalog{ProductID: category, StockQty: qty})
+		if err == nil {
+			break
+		}
+
+		// Log the retry and sleep before the next attempt
+		log.Printf("Retrying UpdateCatalog from DB, attempt %d", retry+1)
+		time.Sleep(time.Second * time.Duration(retry+1))
+	}
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
